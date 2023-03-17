a133baef75b14c9b2edd69c0ccc2aaf8bb70421b
MUHAMMAD SHAHAJAM <mdshahazam6@gmail.com>
2023-03-17 12:28:24 +0530

    Updates

---
+++ precache-manifest.42bc265e994c386d2d30bd83d51086a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34ead9deb1637373ec9e",
+    "url": "/survey-tiger/static/css/main.0d053152.chunk.css"
+  },
+  {
+    "revision": "34ead9deb1637373ec9e",
+    "url": "/survey-tiger/static/js/main.c49f5ef1.chunk.js"
+  },
+  {
+    "revision": "32f8e5e3b28115775edc",
+    "url": "/survey-tiger/static/js/runtime~main.92e11492.js"
+  },
+  {
+    "revision": "640db01bab5d128b60d7",
+    "url": "/survey-tiger/static/js/2.0429b19a.chunk.js"
+  },
+  {
+    "revision": "b077e869bc0ccdb7e074e3b2aa267221",
+    "url": "/survey-tiger/index.html"
+  }
+];
